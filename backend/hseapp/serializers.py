bd5a6b7505304c420db8a228d807904e6ca642df
IDMS JPPP <95725607+IDMSJPPP@users.noreply.github.com>
2023-04-09 23:45:36 +0800

    stuff from git hub

--- backend/hseapp/serializers.py
+++ backend/hseapp/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Staff , Incident
+from django.contrib.auth import get_user_model, authenticate
+from django.core.exceptions import ValidationError
 # from .models import Staff
 
 class StaffSeriallizer(serializers.ModelSerializer):
@@ -17,3 +19,30 @@ class IncidentSeriallizer(serializers.ModelSerializer):
         rep = super(IncidentSeriallizer, self).to_representation(instance)
         rep['name'] = instance.raised_by.name
         return rep
+    
+UserModel = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = UserModel
+		fields = '__all__'
+	def create(self, clean_data):
+		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
+		user_obj.username = clean_data['username']
+		user_obj.save()
+		return user_obj
+
+class UserLoginSerializer(serializers.Serializer):
+	email = serializers.EmailField()
+	password = serializers.CharField()
+	##
+	def check_user(self, clean_data):
+		user = authenticate(username=clean_data['email'], password=clean_data['password'])
+		if not user:
+			raise ValidationError('user not found')
+		return user
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = UserModel
+		fields = ('email', 'username')
--- backend/hseapp/urls.py
+++ backend/hseapp/urls.py
@@ -2,6 +2,8 @@ from .views import StaffViewSet, IncidentViewSet
 # from .views import StaffViewSet
 from rest_framework.routers import DefaultRouter
 from hseapp import views
+from django.urls import path
+
 
 router = DefaultRouter()
 router.register(r'staff', views.StaffViewSet, basename="staff")
@@ -10,6 +12,11 @@ router.register(r'addincident', views.IncidentViewSet, basename="addincident")
 router.register(r'incidenttable', views.IncidentViewSet, basename="incidenttable")
 router.register(r'oneincident', views.IncidentViewSet, basename="oneincident")
 # router.register(r'about', views.IncidentViewSet, basename="about")
+
+path('register', views.UserRegister.as_view(), name='register'),
+path('login', views.UserLogin.as_view(), name='login'),
+path('logout', views.UserLogout.as_view(), name='logout'),
+path('user', views.UserView.as_view(), name='user'),
 
 # router.register(r'incidenttable/oneincident', views.IncidentViewSet, basename="incidenttable/oneincident")
 
