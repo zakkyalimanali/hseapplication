9b79a856cd894b037fb15e24526374a3b6a9769f
IDMS JPPP <95725607+IDMSJPPP@users.noreply.github.com>
2023-05-09 10:21:23 +0800

    new mysql database

--- backend/hseproject/settings.py
+++ backend/hseproject/settings.py
@@ -132,12 +132,24 @@ WSGI_APPLICATION = 'hseproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_name',
+        'USER': 'root',
+        'PASSWORD': 'Crazyno248@',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 
 # DATABASES = {
 #     'default': {
