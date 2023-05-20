e859542e53ba22bfa8633bd83a5d6673b27391b9
IDMS JPPP <95725607+IDMSJPPP@users.noreply.github.com>
2023-05-20 14:04:41 +0800

    meowsd

--- frontend/src/pages/incidentinvestigation/IncidentInvestigationEdit.js
+++ frontend/src/pages/incidentinvestigation/IncidentInvestigationEdit.js
@@ -108,7 +108,194 @@ function IncidentInvestigationEdit() {
 
   return (
     <div className="container mt-5">
-          <div className="row">
+      <h3 className="float-left">Create a New Incident Investigation</h3>
+        <Form onSubmit={onSubmit} className="mt-4">
+      <div className="row">
+      <div className='col-md-6'>
+        <div className="row">
+          <div className="col-md-10 ">
+          <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Investigator</Form.Label>
+                  <Form.Control
+                    as="select"
+                    placeholder="Investigator"
+                    value={investigator}
+                    onChange={(e) => setInvestigator(e.target.value)}
+                  >
+                    <option value=''>Select An Option</option>
+                {staffs.map(staff => {
+                  return <option key={staff.id} value={staff.id}>{staff.name} ({staff.position})</option>
+                })}
+
+                  </Form.Control>
+                </Form.Group>
+              <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Team Member One</Form.Label>
+                  <Form.Control
+                    as="select"
+                    placeholder="Team Member One"
+                    value={team_member_one}
+                    onChange={(e) => setTeamMemberOne(e.target.value)}
+                  >
+                    <option value=''>Select An Option</option>
+                {staffs.map(staff => {
+                  return <option key={staff.id} value={staff.id}>{staff.name} ({staff.position})</option>
+                })}
+
+                  </Form.Control>
+                </Form.Group>
+              <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Team Member Two</Form.Label>
+                  <Form.Control
+                    as="select"
+                    placeholder="Team Member Two"
+                    value={team_member_two}
+                    onChange={(e) => setTeamMemberTwo(e.target.value)}
+                  >
+                    <option value=''>Select An Option</option>
+                {staffs.map(staff => {
+                  return <option key={staff.id} value={staff.id}>{staff.name} ({staff.position})</option>
+                })}
+
+                  </Form.Control>
+                </Form.Group>
+              <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Team Member Three</Form.Label>
+                  <Form.Control
+                    as="select"
+                    placeholder="Team Member Three"
+                    value={team_member_three}
+                    onChange={(e) => setTeamMemberThree(e.target.value)}
+                  >
+                    <option value=''>Select An Option</option>
+                {staffs.map(staff => {
+                  return <option key={staff.id} value={staff.id}>{staff.name} ({staff.position})</option>
+                })}
+
+                  </Form.Control>
+                </Form.Group>
+              <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Team Member Four</Form.Label>
+                  <Form.Control
+                    as="select"
+                    placeholder="Team Member Four"
+                    value={team_member_four}
+                    onChange={(e) => setTeamMemberFour(e.target.value)}
+                  >
+                    <option value=''>Select An Option</option>
+                {staffs.map(staff => {
+                  return <option key={staff.id} value={staff.id}>{staff.name} ({staff.position})</option>
+                })}
+
+                  </Form.Control>
+                </Form.Group>
+          </div>
+        </div>
+        </div>
+        <div class="col">
+        <div className="row">
+          <div className="col-md-10 ">
+          <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Location of Incident</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Location of Incident"
+                    value={location_of_incident}
+                    onChange={(e) => setLocationOfIncident(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Task Performed</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    placeholder="Task Performed"
+                    value={task_performed}
+                    onChange={(e) => setTaskPerformed(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>What Happened</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    placeholder="What Happened"
+                    value={what_happened}
+                    onChange={(e) => setWhatHappened(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Date of Incident</Form.Label>
+                  <Form.Control
+                    type="date"
+                    placeholder="Date of Incident"
+                    value={date_of_incident}
+                    onChange={(e) => setDateOfIncident(e.target.value)}
+                  />
+                </Form.Group>
+
+                
+
+                
+
+
+
+
+                
+          </div>
+        </div>
+        </div>
+        
+
+      </div>
+
+      {/* <div className="col-md-12">
+      
+      </div> */}
+      <div className="col-md-12">
+        <div className="row">
+
+      <Incidentfactorsadd incidentinvestigation = {params.id}/>    
+      <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Summary of Remedial Action</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    placeholder="Summary of Remedial Action"
+                    value={summary_of_remedial_action}
+                    onChange={(e) => setSummaryOfRemedialAction(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Summary of Incident Investigation</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    placeholder="Summary of Incident Investigation"
+                    value={summary_of_incident_investigation}
+                    onChange={(e) => setSummaryOfIncidentInvestigation(e.target.value)}
+                  />
+                </Form.Group>
+      </div>
+      </div>
+    
+      <div className="mt-3 float-right">
+                <Link to="/incidentinvestigationlist/">
+                  <Button
+                    variant="success"
+                    type="button"
+                    onClick={(e) => onUpdate(id)}
+                    className="mx-2"
+                  >
+                    Update
+                  </Button>
+                </Link>
+                
+              </div>
+        </Form>
+          {/* <div className="row">
             <div className= "col-md-4"></div>
             <div className="col-md-4 ">
               <h3 className="float-left">Create a New Incident Investigation</h3>
@@ -259,27 +446,7 @@ function IncidentInvestigationEdit() {
                 </Form.Group>
 
 
-                {/* <Form.Group className="mb-3" controlId="formName">
-                  <Form.Label>Training Expiry</Form.Label>
-                  <Form.Control
-                    type="date"
-                    placeholder="Training Expiry"
-                    value={training_expiry}
-                    onChange={(e) => setTrainingExpiry(e.target.value)}
-                  />
-                </Form.Group> */}
-                
-{/*                 
-                <div className="mt-3 float-right">
-                  <Button
-                    variant="primary"
-                    type="submit"
-                    onClick={onSubmit}
-                    className="mx-2"
-                  >
-                    Save
-                  </Button>
-                </div> */}
+      
                 <div className="mt-3 float-right">
                 <Link to="/incidentinvestigationlist/">
                   <Button
@@ -296,7 +463,7 @@ function IncidentInvestigationEdit() {
               </Form>    
               
             </div>            
-          </div>
+          </div> */}
           <Table striped bordered hover className='mt-3'>
                 <thead>
                     <tr>
