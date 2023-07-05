f61ee6a94fdb7eddc47777873e67ffd0a9d6df91
IDMS JPPP <95725607+IDMSJPPP@users.noreply.github.com>
2023-07-05 14:02:39 +0800

    homepage indevelopment

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,7 +9,10 @@ import SmartCardColorChart from './charts/SmartCardColorChart'
 import WhatChat from './charts/WhatChartcopy';
 import WhyChart from './charts/WhyChart';
 import LSRChart from './charts/LSRChart';
-
+import safetycard from '../images/SafetyImage.jpg';
+import safetycard2 from '../images/SafetyImage2.jpg';
+import safetycard3 from '../images/SafetyImage3.jpg';
+import { Link } from 'react-router-dom';
 
 
 export default function Home() {
@@ -41,19 +44,111 @@ export default function Home() {
 
 
     return(
-        <div className="pt-5">
-        {/* <h1>Home Page/Dashboard</h1> */}
-        {/* <h3>Total Number of Staff: {staffs.length}</h3>
-        <h3>Total Number of Incidents: {incidents.length}</h3>
-        <h3>(A) Head Protection not worn: {incidents.filter(incident => incident.what_happened === "(A) Head Protection not worn").length}</h3>
-        <h3>(B) Eye protection not worn: {incidents.filter(incident => incident.what_happened === '(B) Eye protection not worn').length}</h3> */}
+    <div className="pt-5">
         <div>
-            
-            <div class="mt-4 p-5 bg-primary text-white rounded">
-                <h1 className="mx-auto">Welcome Back</h1>
+            <div className="mt-4 row  p-5 Introduction">
+                <h1 className="d-flex justify-content-center">Welcome To Zakky Industries</h1>
+                <br/>
+                <h3 className="mt-3 d-flex justify-content-center">The Future of Health And Safety is Digital</h3>
+            </div>
+            <div className="row  d-flex justify-content-around">
+                {/* <div className="col-md-4">
+                    <div className='container mt-5'>
+                        <div className="card main-card" >
+                            <img className="card-img-top card-image" src={safetycard} alt="card-image"/>
+                            <div className="card-body">
+                                <h4 className="card-title">Safety Cards</h4>
+                                <p className="card-text">Number of Safety Cards submitted and resolved</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className='container mt-5'>
+                        <div className="card main-card" >
+                            <img className="card-img-top card-image" src={safetycard} alt="card-image"/>
+                            <div className="card-body">
+                                <h4 className="card-title">Safety Cards</h4>
+                                <p className="card-text">Number of Safety Cards submitted and resolved</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className='container mt-5'>
+                        <div className="card main-card" >
+                            <img className="card-img-top card-image" src={safetycard} alt="card-image"/>
+                            <div className="card-body">
+                                <h4 className="card-title">Safety Cards</h4>
+                                <p className="card-text">Number of Safety Cards submitted and resolved</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+                <div className="col-md-3 mt-5 pb-5">
+                    <div className="card">
+                        <img className="card-image" src={safetycard} alt="Avatar"/>
+                        <div class="container">
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            <Link className="mt-2 d-flex justify-content-center" to="incidenttable"><h3><b>Safety Cards</b></h3></Link>
+                            <p className="mt-2">Here you will be able to see how many safety have been submit, by whom and if they have been resolved</p>
+
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mt-5 pb-5">
+                    <div className="card">
+                        <img className="card-image" src={safetycard2} alt="Avatar"/>
+                        <div class="container">
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            <Link className="mt-2 d-flex justify-content-center" to="sitevisitlist"><h3><b>Site Vist</b></h3></Link>
+                            <p className="mt-2">Here you will be able to check on site visits, where they have been conducted, by whom and if they have been when they were done</p>
+
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 mt-5 pb-5">
+                    <div className="card">
+                        <img className="card-image" src={safetycard3} alt="Avatar"/>
+                        <div class="container">
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            {/* <h4 className="d-flex justify-content-center"><b><a href="">Safety Cards</a></b></h4> */}
+                            <Link className="mt-2 d-flex justify-content-center" to="incidentinvestigationlist"><h3><b>Incident Investigation</b></h3></Link>
+                            <p className="mt-2">Here you will be able to see how many incident investigations have been done, by whom and if they have been resolved</p>
+
+                            
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="col-md-3 mt-5 pb-5">
+                    <div className="card">
+                        <img className="card-image" src={safetycard} alt="Avatar"/>
+                        <div class="container">
+                            <h4><b>John Doe</b></h4>
+                            <p>Architect & Engineer</p>
+                        </div>
+                    </div>
+                </div> */}
+                {/* <div className="col-md-3 mt-5 pb-5">
+                    <div className="card">
+                        <img className="card-image" src={safetycard} alt="Avatar"/>
+                        <div class="container">
+                            <h4><b>John Doe</b></h4>
+                            <p>Architect & Engineer</p>
+                        </div>
+                    </div>
+                </div>
+                 */}
             </div>
         </div>
 
-        </div>
+    </div>
     )
 }
